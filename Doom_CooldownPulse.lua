9610310e6ea26f5c1b0c9ad40aa7594a2844205b
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2023-04-09 16:38:23 +0200

    Renamed one of the options to be more consistent and made step increments smaller for "remainingCooldownWhenNotified" variable

--- Doom_CooldownPulse.lua
+++ Doom_CooldownPulse.lua
@@ -376,7 +376,7 @@ function DCP:CreateOptionsFrame()
         { text = "Icon Size", value = "iconSize", min = 30, max = 125, step = 5 },
         { text = "Fade In Time", value = "fadeInTime", min = 0, max = 1.5, step = 0.1 },
         { text = "Fade Out Time", value = "fadeOutTime", min = 0, max = 1.5, step = 0.1 },
-        { text = "Show Before Available (Seconds)", value = "remainingCooldownWhenNotified", min = 0, max = 3, step = 1 },
+        { text = "Show Before Available Time", value = "remainingCooldownWhenNotified", min = 0, max = 3, step = 0.1 },
         { text = "Max Opacity", value = "maxAlpha", min = 0, max = 1, step = 0.1 },
         { text = "Max Opacity Hold Time", value = "holdTime", min = 0, max = 1.5, step = 0.1 },
         { text = "Animation Scaling", value = "animScale", min = 0, max = 2, step = 0.1 },
@@ -432,7 +432,7 @@ function DCP:CreateOptionsFrame()
       tile=1, tileSize=32, edgeSize=32,
       insets={left=11, right=12, top=12, bottom=11}
     })
-    optionsframe:SetWidth(300)
+    optionsframe:SetWidth(260)
     optionsframe:SetHeight(600)
     optionsframe:SetPoint("CENTER",UIParent)
     optionsframe:EnableMouse(true)
